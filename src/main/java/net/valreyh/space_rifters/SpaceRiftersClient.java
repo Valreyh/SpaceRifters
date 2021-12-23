da5fa5c231c9a36823881d84e074fb04021d78dd
Hugman <contact.hugman@gmail.com>
2021-12-23 16:11:16 +0100

    Fixed client class

--- src/main/java/net/valreyh/space_rifters/SpaceRiftersClient.java
+++ src/main/java/net/valreyh/space_rifters/SpaceRiftersClient.java
@@ -1,13 +1,15 @@
 package net.valreyh.space_rifters;
 
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
 import net.valreyh.space_rifters.handlers.CrusherScreen;
 
 @Environment(EnvType.CLIENT)
-public class SpaceRiftersClient {
-    public static void init() {
+public class SpaceRiftersClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
         ScreenRegistry.register(SpaceRifters.CRUSHER_SCREEN_HANDLER, CrusherScreen::new);
     }
 }
