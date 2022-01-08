3656af474f0bcc81d4ab89c1871769ba2824c592
Valreyh <valreyh.pro@gmail.com>
2022-01-08 15:15:43 +0100

    Update SpaceRifters.java

--- src/main/java/net/valreyh/space_rifters/SpaceRifters.java
+++ src/main/java/net/valreyh/space_rifters/SpaceRifters.java
@@ -4,12 +4,27 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.screenhandler.v1.ScreenHandlerRegistry;
+import net.minecraft.block.BlastFurnaceBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.FurnaceBlock;
 import net.minecraft.block.Material;
+import net.minecraft.block.entity.BlastFurnaceBlockEntity;
+import net.minecraft.block.entity.FurnaceBlockEntity;
+import net.minecraft.client.gui.screen.ingame.BlastFurnaceScreen;
+import net.minecraft.client.gui.screen.ingame.FurnaceScreen;
+import net.minecraft.client.gui.screen.recipebook.BlastFurnaceRecipeBookScreen;
+import net.minecraft.client.gui.screen.recipebook.FurnaceRecipeBookScreen;
+import net.minecraft.datafixer.fix.FurnaceRecipesFix;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.loot.function.FurnaceSmeltLootFunction;
+import net.minecraft.recipe.BlastingRecipe;
+import net.minecraft.screen.BlastFurnaceScreenHandler;
+import net.minecraft.screen.FurnaceScreenHandler;
 import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.screen.slot.FurnaceFuelSlot;
+import net.minecraft.screen.slot.FurnaceOutputSlot;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.valreyh.space_rifters.blocks.CrusherBlock;
@@ -25,11 +40,14 @@ public class SpaceRifters implements ModInitializer {
 
 	// BLOCKS //
 	public static final Block LUMOS_ORE = new Block(FabricBlockSettings.of(Material.AMETHYST).strength(4.0f));
+	public static final Block METEOR_ORE = new Block(FabricBlockSettings.of(Material.METAL).strength(4,3));
 	public static final Block CRUSHER = new CrusherBlock(FabricBlockSettings.of(Material.METAL).strength(4.0f));
 	public static final Block LUMOS_STATION = new LumosStationBlock(FabricBlockSettings.of(Material.WOOD).strength(1.0f));
 	// ITEMS //
 	public static final Item HAMMER = new Item(new FabricItemSettings().group(ItemGroup.TOOLS).maxDamage(10));
+
 	public static final Item LUMOS_CRAFTING_CORE = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+	public static final Item VOLCANO_ORB = new Item(new FabricItemSettings().group(ItemGroup.MISC));
 
 	public static final Item LUMOS_FRAGMENT = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
 	public static final Item LUMOS = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
@@ -37,6 +55,7 @@ public class SpaceRifters implements ModInitializer {
 	public static final Item RALIOS = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
 	public static final Item NARSTONE = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
 	public static final Item TEAR_OF_LIGHT = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
+	public static final Item METEOR_FRAGMENT = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
 	// TOOLS //
 	// SCREEN HANDLER //
 	public static ScreenHandlerType<CrusherScreenHandler> CRUSHER_SCREEN_HANDLER = ScreenHandlerRegistry.registerSimple(new Identifier("space_rifters", "crusher_screen"), CrusherScreenHandler::new);
@@ -48,6 +67,7 @@ public class SpaceRifters implements ModInitializer {
 		Registry.register(Registry.BLOCK, id("lumos_ore"), LUMOS_ORE);
 		Registry.register(Registry.BLOCK, id("crusher"), CRUSHER);
 		Registry.register(Registry.BLOCK, id("lumos_station"), LUMOS_STATION);
+		Registry.register(Registry.BLOCK, id("meteor_ore"), METEOR_ORE);
 		// REGISTER ITEMS //
 		Registry.register(Registry.ITEM, id("lumos_fragment"), LUMOS_FRAGMENT);
 		Registry.register(Registry.ITEM, id("lumos"), LUMOS);
@@ -55,13 +75,18 @@ public class SpaceRifters implements ModInitializer {
 		Registry.register(Registry.ITEM, id("ralios"), RALIOS);
 		Registry.register(Registry.ITEM, id("narstone"), NARSTONE);
 		Registry.register(Registry.ITEM, id("tear_of_light"), TEAR_OF_LIGHT);
+		Registry.register(Registry.ITEM, id("meteor_fragment"), METEOR_FRAGMENT);
 
 		Registry.register(Registry.ITEM, id("lumos_ore"), new BlockItem(LUMOS_ORE, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.ITEM, id("crusher"), new BlockItem(CRUSHER, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.ITEM, id("lumos_station"), new BlockItem(LUMOS_STATION, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM, id("meteor_ore"), new BlockItem(METEOR_ORE, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 
 		Registry.register(Registry.ITEM, id("hammer"), HAMMER);
+
 		Registry.register(Registry.ITEM, id("lumos_crafting_core"), LUMOS_CRAFTING_CORE);
+		Registry.register(Registry.ITEM, id("volcano_orb"), VOLCANO_ORB);
+
 	}
 
 	public static Identifier id(String n) {
