6a459b4b9b8250263905b38bee4a5b2db191206d
Valreyh <valreyh.pro@gmail.com>
2022-01-08 18:31:24 +0100

    Fixing Recipe Serialiazer and add ralios craft

--- src/main/java/net/valreyh/space_rifters/objects/recipe/SpaceRiftersRecipe.java
+++ src/main/java/net/valreyh/space_rifters/objects/recipe/SpaceRiftersRecipe.java
@@ -1,43 +1,230 @@
 package net.valreyh.space_rifters.objects.recipe;
 
-import net.minecraft.data.server.recipe.ShapedRecipeJsonFactory;
-import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.inventory.Inventory;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import com.google.gson.*;
+import net.minecraft.inventory.CraftingInventory;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.*;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.JsonHelper;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.world.World;
 import net.valreyh.space_rifters.objects.recipe.serializer.SpaceRiftersRecipeSerializer;
 
-public class SpaceRiftersRecipe implements Recipe<Inventory> {
-    private final Ingredient inputA;
-    private final Ingredient inputB;
-    private final ItemStack outputStack;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import static net.minecraft.recipe.ShapedRecipe.getItem;
+
+public class SpaceRiftersRecipe implements Recipe<CraftingInventory> {
+    public static int height;
+    public static int width;
+    public static DefaultedList<Ingredient> input;
+    public static ItemStack outputStack;
     private final Identifier id;
 
-    public SpaceRiftersRecipe(Ingredient inputA, Ingredient inputB, ItemStack outputStack, Identifier id) {
-        this.inputA = inputA;
-        this.inputB = inputB;
-        this.outputStack = outputStack;
+    public SpaceRiftersRecipe(int width, int height, DefaultedList<Ingredient> input, ItemStack outputStack, Identifier id) {
+        SpaceRiftersRecipe.input = input;
+        SpaceRiftersRecipe.width = width;
+        SpaceRiftersRecipe.height = height;
+        SpaceRiftersRecipe.outputStack = outputStack;
         this.id = id;
     }
 
-    public Ingredient getInputA() {
-        return inputA;
+    public DefaultedList<Ingredient> getIngredients() {
+        return input;
     }
 
-    public Ingredient getInputB() {
-        return inputB;
+    public boolean matches(CraftingInventory craftingInventory, World world) {
+        for(int i = 0; i <= craftingInventory.getWidth() - width; ++i) {
+            for(int j = 0; j <= craftingInventory.getHeight() - height; ++j) {
+                if (this.matchesPattern(craftingInventory, i, j, true)) {
+                    return true;
+                }
+
+                if (this.matchesPattern(craftingInventory, i, j, false)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean matchesPattern(CraftingInventory inv, int offsetX, int offsetY, boolean flipped) {
+        for(int i = 0; i < inv.getWidth(); ++i) {
+            for(int j = 0; j < inv.getHeight(); ++j) {
+                int k = i - offsetX;
+                int l = j - offsetY;
+                Ingredient ingredient = Ingredient.EMPTY;
+                if (k >= 0 && l >= 0 && k < width && l < height) {
+                    if (flipped) {
+                        ingredient = input.get(width - k - 1 + l * width);
+                    } else {
+                        ingredient = input.get(k + l * width);
+                    }
+                }
+
+                if (!ingredient.test(inv.getStack(i + j * inv.getWidth()))) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public static DefaultedList<Ingredient> createPatternMatrix(String[] pattern, Map<String, Ingredient> symbols, int width, int height) {
+        DefaultedList<Ingredient> defaultedList = DefaultedList.ofSize(width * height, Ingredient.EMPTY);
+        Set<String> set = Sets.newHashSet(symbols.keySet());
+        set.remove(" ");
+        set.remove("[");
+        set.remove("]");
+
+        for(int i = 0; i < pattern.length; ++i) {
+            for(int j = 0; j < pattern[i].length(); ++j) {
+                String string = pattern[i].substring(j, j + 1);
+                Ingredient ingredient = symbols.get(string);
+                if (ingredient == null) {
+                    throw new JsonSyntaxException("Pattern references symbol '" + string + "' but it's not defined in the key");
+                }
+                set.remove(string);
+                defaultedList.set(j + width * i, ingredient);
+            }
+        }
+
+        if (!set.isEmpty()) {
+            throw new JsonSyntaxException("Key defines symbols that aren't used in pattern: " + set);
+        } else {
+            return defaultedList;
+        }
+    }
+
+    @VisibleForTesting
+    public static String[] removePadding(String... pattern) {
+        int i = 2147483647;
+        int j = 0;
+        int k = 0;
+        int l = 0;
+
+        for(int m = 0; m < pattern.length; ++m) {
+            String string = pattern[m];
+            i = Math.min(i, findFirstSymbol(string));
+            int n = findLastSymbol(string);
+            j = Math.max(j, n);
+            if (n < 0) {
+                if (k == m) {
+                    ++k;
+                }
+
+                ++l;
+            } else {
+                l = 0;
+            }
+        }
+
+        if (pattern.length == l) {
+            return new String[0];
+        } else {
+            String[] m = new String[pattern.length - l - k];
+
+            for(int string = 0; string < m.length; ++string) {
+                m[string] = pattern[string + k].substring(i, j + 1);
+            }
+
+            return m;
+        }
+    }
+
+    public static String[] getPattern(JsonArray json) {
+        String[] strings = new String[json.size()];
+        if (strings.length > 3) {
+            throw new JsonSyntaxException("Invalid pattern: too many rows, 3 is maximum");
+        } else if (strings.length == 0) {
+            throw new JsonSyntaxException("Invalid pattern: empty pattern not allowed");
+        } else {
+            for(int i = 0; i < strings.length; ++i) {
+                String string = JsonHelper.asString(json.get(i), "pattern[" + i + "]");
+                if (string.length() > 3) {
+                    throw new JsonSyntaxException("Invalid pattern: too many columns, 3 is maximum");
+                }
+
+                if (i > 0 && strings[0].length() != string.length()) {
+                    throw new JsonSyntaxException("Invalid pattern: each row must be the same width");
+                }
+
+                strings[i] = string;
+            }
+
+            return strings;
+        }
+    }
+
+    private static int findFirstSymbol(String line) {
+        int i;
+        for(i = 0; i < line.length() && line.charAt(i) == ' '; ++i) {
+        }
+
+        return i;
+    }
+
+    private static int findLastSymbol(String pattern) {
+        int i;
+        for(i = pattern.length() - 1; i >= 0 && pattern.charAt(i) == ' '; --i) {
+        }
+
+        return i;
+    }
+
+    public static ItemStack outputFromJson(JsonObject json) {
+        Item item = getItem(json);
+        if (json.has("data")) {
+            throw new JsonParseException("Disallowed data tag found");
+        } else {
+            int i = JsonHelper.getInt(json, "count", 1);
+            if (i < 1) {
+                throw new JsonSyntaxException("Invalid output count: " + i);
+            } else {
+                return new ItemStack(item, i);
+            }
+        }
+    }
+
+    public static Map<String, Ingredient> readSymbols(JsonObject json) {
+        Map<String, Ingredient> map = Maps.newHashMap();
+        Iterator<Map.Entry<String, JsonElement>> var2 = json.entrySet().iterator();
+
+        while(var2.hasNext()) {
+            Map.Entry<String, JsonElement> entry = var2.next();
+            if (entry.getKey().length() != 1) {
+                throw new JsonSyntaxException("Invalid key entry: '" + entry.getKey() + "' is an invalid symbol (must be 1 character only).");
+            }
+
+            if (" ".equals(entry.getKey())) {
+                throw new JsonSyntaxException("Invalid key entry: ' ' is a reserved symbol.");
+            }
+
+            map.put(entry.getKey(), Ingredient.fromJson(entry.getValue()));
+        }
+
+        map.put(" ", Ingredient.EMPTY);
+        return map;
     }
 
     @Override
-    public boolean matches(Inventory inventory, World world) {
-        if (inventory.size() < 2) return false;
-        return inputA.test(inventory.getStack(0)) && inputB.test(inventory.getStack(1));
-    }
-
-    @Override
-    public ItemStack craft(Inventory inventory) {
+    public ItemStack craft(CraftingInventory Craft) {
         return this.getOutput().copy();
     }
     @Override
--- src/main/java/net/valreyh/space_rifters/objects/recipe/serializer/SpaceRiftersRecipeSerializer.java
+++ src/main/java/net/valreyh/space_rifters/objects/recipe/serializer/SpaceRiftersRecipeSerializer.java
@@ -1,17 +1,17 @@
 package net.valreyh.space_rifters.objects.recipe.serializer;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonSyntaxException;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.RecipeSerializer;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-import net.valreyh.space_rifters.objects.recipe.SpaceRiftersJsonFormat;
+import net.minecraft.util.JsonHelper;
+import net.minecraft.util.collection.DefaultedList;
 import net.valreyh.space_rifters.objects.recipe.SpaceRiftersRecipe;
+
+import java.util.Iterator;
+import java.util.Map;
 
 public class SpaceRiftersRecipeSerializer implements RecipeSerializer<SpaceRiftersRecipe> {
     // Define ExampleRecipeSerializer as a singleton by making its constructor private and exposing an instance.
@@ -21,44 +21,46 @@ public class SpaceRiftersRecipeSerializer implements RecipeSerializer<SpaceRifte
     public static final SpaceRiftersRecipeSerializer INSTANCE = new SpaceRiftersRecipeSerializer();
 
     // This will be the "type" field in the json
-    public static final Identifier ID = new Identifier("space_rifters:spacerifters_recipe");
+    public static final Identifier ID = new Identifier("space_rifters:recipe");
 
     @Override
     // Turns json into Recipe
     public SpaceRiftersRecipe read(Identifier id, JsonObject json) {
-
-        SpaceRiftersJsonFormat recipeJson = new Gson().fromJson(json, SpaceRiftersJsonFormat.class);
-
-        // Validate all fields are there
-        if (recipeJson.inputA == null || recipeJson.inputB == null || recipeJson.outputItem == null) {
-            throw new JsonSyntaxException("A required attribute is missing!");
-        }
-        // We'll allow to not specify the output, and default it to 1.
-        if (recipeJson.outputAmount == 0) recipeJson.outputAmount = 1;
-
-        Ingredient inputA = Ingredient.fromJson(recipeJson.inputA);
-        Ingredient inputB = Ingredient.fromJson(recipeJson.inputB);
-        Item outputItem = Registry.ITEM.getOrEmpty(new Identifier(recipeJson.outputItem))
-                // Validate the inputted item actually exists
-                .orElseThrow(() -> new JsonSyntaxException("No such item " + recipeJson.outputItem));
-        ItemStack output = new ItemStack(outputItem, recipeJson.outputAmount);
-
-        return new SpaceRiftersRecipe(inputA, inputB, output, id);
+        Map<String, Ingredient> map = SpaceRiftersRecipe.readSymbols(JsonHelper.getObject(json, "key"));
+        String[] strings = SpaceRiftersRecipe.removePadding(SpaceRiftersRecipe.getPattern(JsonHelper.getArray(json, "pattern")));
+        int i = strings[0].length();
+        int j = strings.length;
+        DefaultedList<Ingredient> defaultedList = SpaceRiftersRecipe.createPatternMatrix(strings, map, i, j);
+        ItemStack itemStack = SpaceRiftersRecipe.outputFromJson(JsonHelper.getObject(json, "result"));
+        return new SpaceRiftersRecipe(i,j,defaultedList,itemStack,id);
     }
 
     @Override
     public void write(PacketByteBuf packetData, SpaceRiftersRecipe recipe) {
-        recipe.getInputA().write(packetData);
-        recipe.getInputB().write(packetData);
-        packetData.writeItemStack(recipe.getOutput());
+        packetData.writeVarInt(SpaceRiftersRecipe.width);
+        packetData.writeVarInt(SpaceRiftersRecipe.height);
+        Iterator<Ingredient> var3 = SpaceRiftersRecipe.input.iterator();
+
+        while(var3.hasNext()) {
+            Ingredient ingredient = var3.next();
+            ingredient.write(packetData);
+        }
+
+        packetData.writeItemStack(SpaceRiftersRecipe.outputStack);
     }
 
     @Override
     public SpaceRiftersRecipe read(Identifier recipeId, PacketByteBuf packetData) {
-        // Make sure the read in the same order you have written!
-        Ingredient inputA = Ingredient.fromPacket(packetData);
-        Ingredient inputB = Ingredient.fromPacket(packetData);
+        int i = packetData.readVarInt();
+        int j = packetData.readVarInt();
+        DefaultedList<Ingredient> defaultedList = DefaultedList.ofSize(i * j, Ingredient.EMPTY);
         ItemStack output = packetData.readItemStack();
-        return new SpaceRiftersRecipe(inputA, inputB, output, recipeId);
+
+        for(int k = 0; k < defaultedList.size(); ++k) {
+            defaultedList.set(k, Ingredient.fromPacket(packetData));
+        }
+
+        ItemStack k = packetData.readItemStack();
+        return new SpaceRiftersRecipe(i,j,defaultedList,output,recipeId);
     }
 }
